fc626f0a7dfefe7d84bf523e03f6840e31ae05e6
jesussiancasf <jesussianflo@gmail.com>
2022-03-20 19:42:55 -0500

    encapsulando los atributos de la clase Patient

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,7 @@ public class Main {
         System.out.println(myDoctor.speciality);
 
         Patient patient=new Patient("Alejandro Quevedo","alequeve@gmail.com");
-        System.out.println(patient.name);
-        System.out.println(patient.email);
 
-        patient.weight=60.5;//kg
-        patient.height=1.65;//m
-
-        System.out.println(patient.weight);
-        System.out.println(patient.height);
     }
 
 
